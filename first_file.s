a24e5082987b48f79a99c2817263b38bb448003a
suvadityamuk <suvadityamuk@gmail.com>
2021-07-19 16:22:15 +0530

    New commit with right files again.

--- first_file.s
+++ first_file.s
@@ -1,116 +1,64 @@
 	.file	"first_file.cpp"
 	.text
-	.section .rdata,"dr"
-_ZStL19piecewise_construct:
-	.space 1
-.lcomm _ZStL8__ioinit,1,1
+	.p2align 4
+	.def	__tcf_0;	.scl	3;	.type	32;	.endef
+	.seh_proc	__tcf_0
+__tcf_0:
+.LFB2100:
+	.seh_endprologue
+	leaq	_ZStL8__ioinit(%rip), %rcx
+	jmp	_ZNSt8ios_base4InitD1Ev
+	.seh_endproc
 	.def	__main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
 	.align 8
 .LC0:
 	.ascii "Hello, this is Suvaditya's first file.\0"
-	.text
+	.section	.text.startup,"x"
+	.p2align 4
 	.globl	main
 	.def	main;	.scl	2;	.type	32;	.endef
 	.seh_proc	main
 main:
-.LFB1585:
-	pushq	%rbp
-	.seh_pushreg	%rbp
-	movq	%rsp, %rbp
-	.seh_setframe	%rbp, 0
-	subq	$32, %rsp
-	.seh_stackalloc	32
+.LFB1611:
+	subq	$40, %rsp
+	.seh_stackalloc	40
 	.seh_endprologue
 	call	__main
-	leaq	.LC0(%rip), %rdx
 	movq	.refptr._ZSt4cout(%rip), %rcx
-	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
-	movq	.refptr._ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_(%rip), %rdx
-	movq	%rax, %rcx
-	call	_ZNSolsEPFRSoS_E
-	movl	$0, %eax
-	addq	$32, %rsp
-	popq	%rbp
+	movl	$38, %r8d
+	leaq	.LC0(%rip), %rdx
+	call	_ZSt16__ostream_insertIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_PKS3_x
+	movq	.refptr._ZSt4cout(%rip), %rcx
+	call	_ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_
+	xorl	%eax, %eax
+	addq	$40, %rsp
 	ret
 	.seh_endproc
-	.def	__tcf_0;	.scl	3;	.type	32;	.endef
-	.seh_proc	__tcf_0
-__tcf_0:
-.LFB2075:
-	pushq	%rbp
-	.seh_pushreg	%rbp
-	movq	%rsp, %rbp
-	.seh_setframe	%rbp, 0
-	subq	$32, %rsp
-	.seh_stackalloc	32
-	.seh_endprologue
-	leaq	_ZStL8__ioinit(%rip), %rcx
-	call	_ZNSt8ios_base4InitD1Ev
-	nop
-	addq	$32, %rsp
-	popq	%rbp
-	ret
-	.seh_endproc
-	.def	_Z41__static_initialization_and_destruction_0ii;	.scl	3;	.type	32;	.endef
-	.seh_proc	_Z41__static_initialization_and_destruction_0ii
-_Z41__static_initialization_and_destruction_0ii:
-.LFB2074:
-	pushq	%rbp
-	.seh_pushreg	%rbp
-	movq	%rsp, %rbp
-	.seh_setframe	%rbp, 0
-	subq	$32, %rsp
-	.seh_stackalloc	32
-	.seh_endprologue
-	movl	%ecx, 16(%rbp)
-	movl	%edx, 24(%rbp)
-	cmpl	$1, 16(%rbp)
-	jne	.L6
-	cmpl	$65535, 24(%rbp)
-	jne	.L6
-	leaq	_ZStL8__ioinit(%rip), %rcx
-	call	_ZNSt8ios_base4InitC1Ev
-	leaq	__tcf_0(%rip), %rcx
-	call	atexit
-.L6:
-	nop
-	addq	$32, %rsp
-	popq	%rbp
-	ret
-	.seh_endproc
+	.p2align 4
 	.def	_GLOBAL__sub_I_main;	.scl	3;	.type	32;	.endef
 	.seh_proc	_GLOBAL__sub_I_main
 _GLOBAL__sub_I_main:
-.LFB2076:
-	pushq	%rbp
-	.seh_pushreg	%rbp
-	movq	%rsp, %rbp
-	.seh_setframe	%rbp, 0
-	subq	$32, %rsp
-	.seh_stackalloc	32
+.LFB2101:
+	subq	$40, %rsp
+	.seh_stackalloc	40
 	.seh_endprologue
-	movl	$65535, %edx
-	movl	$1, %ecx
-	call	_Z41__static_initialization_and_destruction_0ii
-	nop
-	addq	$32, %rsp
-	popq	%rbp
-	ret
+	leaq	_ZStL8__ioinit(%rip), %rcx
+	call	_ZNSt8ios_base4InitC1Ev
+	leaq	__tcf_0(%rip), %rcx
+	addq	$40, %rsp
+	jmp	atexit
 	.seh_endproc
 	.section	.ctors,"w"
 	.align 8
 	.quad	_GLOBAL__sub_I_main
+.lcomm _ZStL8__ioinit,1,1
 	.ident	"GCC: (tdm64-1) 9.2.0"
-	.def	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc;	.scl	2;	.type	32;	.endef
-	.def	_ZNSolsEPFRSoS_E;	.scl	2;	.type	32;	.endef
 	.def	_ZNSt8ios_base4InitD1Ev;	.scl	2;	.type	32;	.endef
+	.def	_ZSt16__ostream_insertIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_PKS3_x;	.scl	2;	.type	32;	.endef
+	.def	_ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_;	.scl	2;	.type	32;	.endef
 	.def	_ZNSt8ios_base4InitC1Ev;	.scl	2;	.type	32;	.endef
 	.def	atexit;	.scl	2;	.type	32;	.endef
-	.section	.rdata$.refptr._ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_, "dr"
-	.globl	.refptr._ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_
-	.linkonce	discard
-.refptr._ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_:
-	.quad	_ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_
 	.section	.rdata$.refptr._ZSt4cout, "dr"
 	.globl	.refptr._ZSt4cout
 	.linkonce	discard
